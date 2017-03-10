7353dbf3215b134d38b3af4a57188ef1c1a0a830
khjang <zingahu@gmail.com>
2017-03-10 18:34:21 +0900

    index.jsp 에서 사용할 빈 생성

---
+++ src/com/board/beans/Board.java
@@ -0,0 +1,46 @@
+package com.board.beans;
+
+public class Board {
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	public String getWrite() {
+		return write;
+	}
+	public void setWrite(String write) {
+		this.write = write;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	private int idx;
+	private String title;
+	private String regdate;
+	private String write;
+	private int count;
+	private String content;
+}
